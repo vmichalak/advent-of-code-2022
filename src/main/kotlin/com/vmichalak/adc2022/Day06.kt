773ebaf80d5f71f9480700b89221e56fb81d8e06
Valentin Michalak <valentinmichalak@gmail.com>
2022-12-07 00:30:47 +0100

    Day 6 - Optimized Version O(n)

--- src/main/kotlin/com/vmichalak/adc2022/Day06.kt
+++ src/main/kotlin/com/vmichalak/adc2022/Day06.kt
@@ -5,10 +5,23 @@ package com.vmichalak.adc2022
  * https://adventofcode.com/2022/day/6
  */
 object Day06 {
-    private fun String.allUnique(): Boolean = all(hashSetOf<Char>()::add)
+    private fun Char.toArrayIndex() = this - 'a'
 
-    private fun findMarker(content: String, markerSize: Int) =
-        content.windowedSequence(markerSize).indexOfFirst { it.allUnique() } + markerSize
+    private fun IntArray.getAndReplace(index: Int, value: Int): Int {
+        val oldValue = this[index]
+        this[index] = value
+        return oldValue
+    }
+
+    private fun findMarker(content: String, markerSize: Int): Int {
+        val indexArray = IntArray(26)
+        var lastDuplicateIndex = 0
+        return content.withIndex().indexOfFirst { c ->
+            val lastIndex = indexArray.getAndReplace(c.value.toArrayIndex(), c.index)
+            lastDuplicateIndex = lastDuplicateIndex.coerceAtLeast(lastIndex)
+            c.index - lastDuplicateIndex >= markerSize
+        } + 1
+    }
 
     fun part1(input: String): Int = findMarker(input, 4)
     fun part2(input: String): Int = findMarker(input, 14)
